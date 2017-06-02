16f6239b255ebbba38459ae5b91daba54a1db65e
JoSuzuki <jonathan@suzuki.pro.br>
2017-06-02 19:36:47 -0300

    Made new myform.cpp

--- Mantenikola/Mantenikola/MyForm.cpp
+++ Mantenikola/Mantenikola/MyForm.cpp
@@ -1,13 +1,15 @@
 #include "MyForm.h"
+
 using namespace System;
 using namespace System::Windows::Forms;
- 
+
 
 [STAThread]
 void Main(array<String^>^ args)
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-	Aula::MyForm form;
-	Application::Run(%form);	
+
+	Mantenikola::MyForm form;
+	Application::Run(%form);
 }
--- Mantenikola/Mantenikola/MyForm.h
+++ Mantenikola/Mantenikola/MyForm.h
@@ -1,8 +1,6 @@
 #pragma once
-#include "AlunoDAO.h"
-#include "Aluno.h"
-#include <msclr\marshal_cppstd.h>
-namespace Aula {
+
+namespace Mantenikola {
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -36,18 +34,6 @@ namespace Aula {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^  button1;
-	protected:
-	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::Label^  label2;
-	private: System::Windows::Forms::Label^  label3;
-	private: System::Windows::Forms::TextBox^  textBox1;
-	private: System::Windows::Forms::TextBox^  textBox2;
-	private: System::Windows::Forms::Button^  button2;
-	private: System::Windows::Forms::ListView^  listView1;
-	private: System::Windows::Forms::Label^  label4;
-	private: System::Windows::Forms::ColumnHeader^  columnHeader1;
-	private: System::Windows::Forms::ColumnHeader^  columnHeader2;
 
 	private:
 		/// <summary>
@@ -62,153 +48,12 @@ namespace Aula {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::Windows::Forms::ListViewItem^  listViewItem1 = (gcnew System::Windows::Forms::ListViewItem(L"Aluno0"));
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->listView1 = (gcnew System::Windows::Forms::ListView());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->columnHeader1 = (gcnew System::Windows::Forms::ColumnHeader());
-			this->columnHeader2 = (gcnew System::Windows::Forms::ColumnHeader());
-			this->SuspendLayout();
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(276, 32);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"Consultar";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(47, 9);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(131, 13);
-			this->label1->TabIndex = 1;
-			this->label1->Text = L"Seleciona o Aluno de ID 1";
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(50, 42);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(35, 13);
-			this->label2->TabIndex = 2;
-			this->label2->Text = L"Nome";
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(142, 42);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(69, 13);
-			this->label3->TabIndex = 3;
-			this->label3->Text = L"Número USP";
-			// 
-			// textBox1
-			// 
-			this->textBox1->Location = System::Drawing::Point(53, 59);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(78, 20);
-			this->textBox1->TabIndex = 4;
-			// 
-			// textBox2
-			// 
-			this->textBox2->Location = System::Drawing::Point(145, 58);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(66, 20);
-			this->textBox2->TabIndex = 5;
-			// 
-			// button2
-			// 
-			this->button2->Location = System::Drawing::Point(276, 61);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(75, 23);
-			this->button2->TabIndex = 6;
-			this->button2->Text = L"Atualiz";
-			this->button2->UseVisualStyleBackColor = true;
-			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
-			// 
-			// listView1
-			// 
-			this->listView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::ColumnHeader^  >(2) { this->columnHeader1, this->columnHeader2 });
-			this->listView1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ListViewItem^  >(1) { listViewItem1 });
-			this->listView1->Location = System::Drawing::Point(41, 112);
-			this->listView1->Name = L"listView1";
-			this->listView1->Size = System::Drawing::Size(169, 131);
-			this->listView1->TabIndex = 7;
-			this->listView1->UseCompatibleStateImageBehavior = false;
-			// 
-			// label4
-			// 
-			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(44, 94);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(78, 13);
-			this->label4->TabIndex = 8;
-			this->label4->Text = L"Lista de alunos";
-			// 
-			// columnHeader1
-			// 
-			this->columnHeader1->Text = L"Nome";
-			// 
-			// columnHeader2
-			// 
-			this->columnHeader2->Text = L"Numero USP";
-			// 
-			// MyForm
-			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(414, 261);
-			this->Controls->Add(this->label4);
-			this->Controls->Add(this->listView1);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
-			this->Controls->Add(this->button1);
-			this->Name = L"MyForm";
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,300);
 			this->Text = L"MyForm";
-			this->ResumeLayout(false);
-			this->PerformLayout();
-
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-		
-		this->label2->Text = "Aluno";
-		this->label3->Text = "05378441";
-		Aluno * aluno = nullptr;
-		AlunoDAO* dao = new AlunoDAO();
-		aluno = dao->getAluno(2);
-		this->label2->Text = gcnew System::String(aluno->getNome().c_str());
-		this->label3->Text = gcnew System::String(aluno->getNumeroUsp().c_str());
-		this->textBox1->Text = gcnew System::String(aluno->getNome().c_str());
-		this->textBox2->Text = gcnew System::String(aluno->getNumeroUsp().c_str());
-
-	}
-	private:
-		System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
-		msclr::interop::marshal_context context;
-		string nome = context.marshal_as<std::string>(this->textBox1->Text);
-		string numeroUsp = context.marshal_as<std::string>(this->textBox2->Text);
-		Aluno * a = new Aluno();
-		a->setNome(nome);
-		a->setNumeroUsp(numeroUsp);
-		AlunoDAO * dao = new AlunoDAO();
-		dao->inserir(a);
-		this->listView1->Items->Add("teste");
-	}
-};
+	};
 }
