99d2974c0292b09cb3b30ca515842edaf66c93f6
POGVaz <pedro.oscar.vaz@usp.br>
2017-06-07 09:52:35 -0300

    MenuPrincipal.ccp com Main certo

--- Mantenikola/Mantenikola/MenuPrincipal.cpp
+++ Mantenikola/Mantenikola/MenuPrincipal.cpp
@@ -1,2 +1,11 @@
 #include "MenuPrincipal.h"
 
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Project1::MenuPrincipal tela;
+	Application::Run(%tela);
+}
